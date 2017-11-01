bed0379ef12effc2f9af96dd928a9d77b499c009
LYC <LYCMAC@huangninghaideimac.local>
2017-11-02 07:05:21 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by LYC on 2017/11/2.
+//  Copyright © 2017年 YCLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by LYC on 2017/11/2.
+//  Copyright © 2017年 YCLY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
